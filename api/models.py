890484142bffbe7647edc99b62e2cd3d9af3521f
Max Malkin <60683392+maxmalkin@users.noreply.github.com>
2024-10-16 18:53:08 -0600

    add Stock model, ensure uniqueness of entries

--- api/models.py
+++ api/models.py
@@ -1,8 +1,20 @@
+# financials/models.py
 from django.db import models
 
-class Item(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
+class StockPrice(models.Model):
+    symbol = models.CharField(max_length=10) 
+    date = models.DateField() 
+    open_price = models.DecimalField(max_digits=10, decimal_places=2)
+    high_price = models.DecimalField(max_digits=10, decimal_places=2)
+    low_price = models.DecimalField(max_digits=10, decimal_places=2)
+    close_price = models.DecimalField(max_digits=10, decimal_places=2)
+    volume = models.BigIntegerField()
+
+    class Meta:
+        unique_together = ('symbol', 'date') 
+        indexes = [
+            models.Index(fields=['symbol', 'date']),  
+        ]
 
     def __str__(self):
-        return self.name
+        return f"{self.symbol} - {self.date}"
